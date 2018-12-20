39d1c0699b3dde8624d9bd69960c88e477f4ce04
Made2Glow <mblade@akitaworks.ca>
2018-12-19 22:29:22 -0700

    Modified some comments.
    Cleaned up the code, repub..

--- SmallAnimations/FastXmas/FastXmas.ino
+++ SmallAnimations/FastXmas/FastXmas.ino
@@ -1,14 +1,21 @@
-// FastXmas twinkle of red/green/white colors randomly.
+// FastXmas a twinkle animation of red/green/white colors randomly.
 // By Chris Alcock (Aka mBlade/Made2Glow) Dec 19, 2018
+// Tested on a Teensy 3.2 and Nano v3 (Atmega 328p)
 
-// This animation was taken from Gem Raptor code.
+// You will require the FastLED library from http://fastled.io/
+// This code is found at https://github.com/Made2Glow/Code/tree/master/SmallAnimations/FastXmas
+
+// Modify or change this code as you see fit. It is for free to use. 
+// Credit would be nice if you post on social media but not required.
+// Twitter: @made2glow
+// Facebook: https://www.facebook.com/made2glow/ (I don't post here at all, but it exists)
 
 #include <FastLED.h>
 
 #define DATA_PIN          2
 #define NUM_LEDS          26
-#define NUM_XMAS_PIXELS   NUM_LEDS/4
 #define BRIGHTNESS        64
+#define NUM_XMAS_PIXELS   NUM_LEDS/4
 
 bool _runonce =           true;
 CRGB leds[NUM_LEDS];
@@ -40,13 +47,13 @@ void xmasmode() {
   }
 
   for (uint16_t i = 0;i < NUM_XMAS_PIXELS; i++) {
-    if (xmas_val[i] == 192) {
+    if (xmas_val[i] == 192) { // 192 is 1 for sin8() which siginifies the end of the glow cycle and choose a new place to glow.
       leds[xmas_pos[i]] = CRGB(0,0,0); // the sin8() function doesn't go right to zero. This ensures we don't go into an infinate loop in the freepos() function.
       xmas_pos[i] = freepos(); // Choose new open position.
       xmas_color[i] = random(3); // randomize the new color.
     }
-    xmas_val[i]++;
-
+    xmas_val[i]++; // Add 1 for ever. after 255 it will wrap back to 0. 
+    // try  += 2 or 3 to speed up the animation. You can also use floats too if you want more control.
     switch (xmas_color[i]) {
       case 0: leds[xmas_pos[i]] = CHSV(0,255,sin8(xmas_val[i])); break;
       case 1: leds[xmas_pos[i]] = CHSV(96,255,sin8(xmas_val[i])); break;
@@ -60,12 +67,12 @@ void xmasmode() {
 void setup() {
   FastLED.addLeds<WS2812,DATA_PIN,GRB>(leds,NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
-  FastLED.show();
+  FastLED.show(); // Clear the strip first.
 }
 
 void loop() {
-  xmasmode();
+  xmasmode(); // run the function.
 
   FastLED.show();
-  delay(10);
+  delay(10); // delay 10ms (100 fps)
 }
